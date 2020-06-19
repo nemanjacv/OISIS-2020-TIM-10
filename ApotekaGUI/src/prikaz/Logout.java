80f41fefc07fa06a1ca1bf44bd4007c980df0468
milutinmilica <62249217+milutinmilica@users.noreply.github.com>
2020-06-19 23:16:18 +0200

    back dugme

--- ApotekaGUI/src/prikaz/Logout.java
+++ ApotekaGUI/src/prikaz/Logout.java
@@ -2,10 +2,13 @@ package prikaz;
 
 import java.awt.Color;
 import java.awt.Cursor;
+import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
@@ -19,7 +22,7 @@ public class Logout extends JPanel {
 	
 
 	private static final long serialVersionUID = -4813329911065574369L;
-	JButton home = new JButton("5");
+	JButton home = new JButton("");
 	public static JButton logout = new JButton("Izloguj se");
 	
 	public Logout() 
@@ -42,6 +45,19 @@ public class Logout extends JPanel {
 				
 			}
 		});
+		  
+
+		home.setPreferredSize(new Dimension(30, 30));
+		ImageIcon btnn1 = new ImageIcon("img/i8.png");
+		Image butn1 = btnn1.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+		btnn1 = new ImageIcon(butn1, btnn1.getDescription());
+
+		home.setIcon(btnn1);
+		home.setBorder(null);
+		home.setFocusable(false);
+		home.setContentAreaFilled(false);
+		home.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+
 		
 		home.addActionListener(new ActionListener() {
 			
