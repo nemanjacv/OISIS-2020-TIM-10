0908ebd62a9957da7c7fafa2d109e952e967bebe
milutinmilica <62249217+milutinmilica@users.noreply.github.com>
2020-08-18 03:04:47 +0200

    Kreiranje recepata

--- ApotekaGUI/src/paneli/DodavanjeReceptaWin.java
+++ ApotekaGUI/src/paneli/DodavanjeReceptaWin.java
@@ -49,15 +49,39 @@ public class DodavanjeReceptaWin extends JPanel{
 	JLabel Podaci = new JLabel("PODACI O PACIJENTU ");
 
 	JPanel jmbg = new JPanel();
-	JLabel JMBG = new JLabel("JMBG    ");
+	JLabel JMBG = new JLabel("JMBG                          ");
 	JTextField JMBGT = new JTextField();
 	
 	JPanel podaci2 = new JPanel();
-	JLabel Podaci2 = new JLabel("OSTALI PODACI  ");
+	JLabel Podaci2 = new JLabel("OSTALI PODACI     ");
 	
 	JPanel datumI = new JPanel();
 	JLabel DatumI = new JLabel("Datum izdavanja    ");
 	JTextField DatumIT = new JTextField();
+	
+	JPanel datumK= new JPanel();
+	JLabel DatumK = new JLabel("Datum kreiranja     ");
+	JTextField DatumKT = new JTextField();
+	
+	JPanel lekovi = new JPanel(); 
+	JLabel Lekovi = new JLabel("LEKOVI           ");
+	
+	JPanel sifra= new JPanel();
+	JLabel Sifra = new JLabel("Sifra                               ");
+	JTextField SifraT = new JTextField();
+	
+	JPanel kolicina= new JPanel();
+	JLabel Kolicina = new JLabel("Kolicina                        ");
+	JTextField KolicinaT = new JTextField();
+	
+	JPanel sifraL= new JPanel();
+	JLabel SifraL = new JLabel("SIFRA LEKARA      ");
+	JTextField SifraLT = new JTextField();
+	
+	JPanel sifraR= new JPanel();
+	JLabel SifraR = new JLabel("SIFRA RECEPTA    ");
+	JTextField SifraRT = new JTextField();
+	
 	
 	public DodavanjeReceptaWin()
 	{	
@@ -77,7 +101,7 @@ public class DodavanjeReceptaWin extends JPanel{
 		
 		izmene.setLayout(new BoxLayout(izmene,BoxLayout.Y_AXIS));
 		izmene.setOpaque( false );
-		izmene.setBorder(new EmptyBorder(70, 0,0, 0));
+		izmene.setBorder(new EmptyBorder(10, 0,0, 0));
 		
 		podaci.setLayout(new FlowLayout(FlowLayout.CENTER));
 		podaci.setOpaque( false );
@@ -98,6 +122,38 @@ public class DodavanjeReceptaWin extends JPanel{
 		datumI.setOpaque( false );
 		DatumI.setOpaque( false );
 		DatumIT.setPreferredSize(new Dimension(150,20));
+		
+		datumK.setLayout(new FlowLayout(FlowLayout.CENTER));
+		datumK.setOpaque( false );
+		DatumK.setOpaque( false );
+		DatumKT.setPreferredSize(new Dimension(150,20));
+		
+		
+		lekovi.setLayout(new FlowLayout(FlowLayout.CENTER));
+		lekovi.setOpaque( false );
+		Lekovi.setOpaque( false );
+		
+		sifra.setLayout(new FlowLayout(FlowLayout.CENTER));
+		sifra.setOpaque( false );
+		Sifra.setOpaque( false );
+		SifraT.setPreferredSize(new Dimension(150,20));
+		
+		kolicina.setLayout(new FlowLayout(FlowLayout.CENTER));
+		kolicina.setOpaque( false );
+		Kolicina.setOpaque( false );
+		KolicinaT.setPreferredSize(new Dimension(150,20));
+		
+		sifraL.setLayout(new FlowLayout(FlowLayout.CENTER));
+		sifraL.setOpaque( false );
+		SifraL.setOpaque( false );
+		SifraLT.setPreferredSize(new Dimension(150,20));
+		
+		sifraL.setBorder(new EmptyBorder(40, 0,0, 0));
+		
+		sifraR.setLayout(new FlowLayout(FlowLayout.CENTER));
+		sifraR.setOpaque( false );
+		SifraR.setOpaque( false );
+		SifraRT.setPreferredSize(new Dimension(150,20));
 
 		
 		
@@ -117,15 +173,54 @@ public class DodavanjeReceptaWin extends JPanel{
 		
 		Podaci2.setFont(new Font("Calibri", Font.PLAIN, 20));
 		Podaci2.setForeground(Color.white);
-		podaci2.add(Podaci);
+		podaci2.add(Podaci2);
 		
 		DatumI.setFont(new Font("Calibri", Font.PLAIN, 15));
 		DatumI.setForeground(Color.white);
 		datumI.add(DatumI);
 		datumI.add(DatumIT);
+		
+		DatumK.setFont(new Font("Calibri", Font.PLAIN, 15));
+		DatumK.setForeground(Color.white);
+		datumK.add(DatumK);
+		datumK.add(DatumKT);
+		
+		Lekovi.setFont(new Font("Calibri", Font.PLAIN, 20));
+		Lekovi.setForeground(Color.white);
+		lekovi.add(Lekovi);
+		
+		Sifra.setFont(new Font("Calibri", Font.PLAIN, 15));
+		Sifra.setForeground(Color.white);
+		sifra.add(Sifra);
+		sifra.add(SifraT);
 	
 		
+		Kolicina.setFont(new Font("Calibri", Font.PLAIN, 15));
+		Kolicina.setForeground(Color.white);
+		kolicina.add(Kolicina);
+		kolicina.add(KolicinaT);
+		
+		SifraL.setFont(new Font("Calibri", Font.PLAIN, 20));
+		SifraL.setForeground(Color.white);
+		sifraL.add(SifraL);
+		sifraL.add(SifraLT);
+		
+		SifraR.setFont(new Font("Calibri", Font.PLAIN, 20));
+		SifraR.setForeground(Color.white);
+		sifraR.add(SifraR);
+		sifraR.add(SifraRT);
+		
+		
+		izmene.add(podaci);
 		izmene.add(jmbg);
+		izmene.add(podaci2);
+		izmene.add(datumI);
+		izmene.add(datumK);
+		izmene.add(lekovi);
+		izmene.add(sifra);
+		izmene.add(kolicina);
+		izmene.add(sifraL);
+		izmene.add(sifraR);
 
 		panel.add(top, BorderLayout.PAGE_START);
 		panel.add(izmene, BorderLayout.CENTER);
